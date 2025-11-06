fee50f1e62477423e272bf72839f5e582d9c80d7
Igor Clark <igorclarkdasilva@outlook.com.br>
2025-11-06 10:28:22 -0300

    Adds Login screen logic

--- js/pages/Login/index.js
+++ js/pages/Login/index.js
@@ -1,6 +1,31 @@
+import { initializeFirebase } from "../../components/DBConnection/firebase.js";
+import { renderPage } from "../../utils.js";
+import { Home } from "../Home/index.js";
+
+const { auth, provider } = initializeFirebase();
+
 export const Login = `
-  <div class="login-container">
-    <h2>Login to House Hub</h2>
-    <button id="google-signin-btn">Sign in with Google</button>
+  <div>
+    <h1>Login</h1>
+    <button id="loginBtn">Entrar com Google</button>
   </div>
 `;
+
+export function initLogin(containerId) {
+  renderPage(containerId, Login);
+
+  const loginBtn = document.getElementById("loginBtn");
+  if (!loginBtn) return;
+
+  loginBtn.addEventListener("click", async () => {
+    try {
+      await auth.signInWithPopup(provider);
+      renderPage(containerId, Home);
+      // se existir initHome, chame aqui
+      // initHome(containerId);
+    } catch (err) {
+      console.error(err);
+      alert("Erro ao logar: " + err.message);
+    }
+  });
+}
--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 import { initializeFirebase, renderPage } from "./js/utils.js";
 import { Pages } from "./js/pages/pages.js";
+import { initLogin } from "./js/pages/Login/index.js";
 
-renderPage("app", Pages.login);
+document.addEventListener("DOMContentLoaded", () => {
+  initializeFirebase();
+  renderPage("app", Pages.login);
+  initLogin("app");
+});
